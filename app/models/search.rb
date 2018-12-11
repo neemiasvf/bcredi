d8b5b53305d624d9346e14c126c4d46574399768
Neemias Freitas <neemiasvf@gmail.com>
2018-12-10 23:31:50 -0200

    Include query terms to searches

--- app/models/search.rb
+++ app/models/search.rb
@@ -23,7 +23,7 @@ class Search < ApplicationRecord
     # it's necessary to search once for each language and then merge all results into one.
    self[:results] = []
     self[:languages].each do |language|
-      git_response = JSON.parse(RestClient.get "#{Search::API_URL}search/repositories?q=language=#{language}&sort=stars", {content_type: :json, accept: :json})
+      git_response = JSON.parse(RestClient.get "#{Search::API_URL}search/repositories?q=#{self[:name]}+language=#{language}&sort=stars", {content_type: :json, accept: :json})
       self[:results] += git_response["items"]
     end
 
